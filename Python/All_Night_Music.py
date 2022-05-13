de47a840498fba7a154320a61292cdce3a42cfcd
seida <k29gox@gmail.com>
2022-05-14 01:46:58 +0900

    main

---
+++ Python/All_Night_Music.py
@@ -0,0 +1,26 @@
+# テレビ東京のAll Night Musicで放送された曲の一覧のHTMLファイルを作成します。
+# 作成されたHTMLファイルから、YouTubeで検索して視聴できます。
+
+from selenium import webdriver
+edge=webdriver.Edge("msedgedriver.exe")
+edge.get("https:www.ttmnet.co.jp/blog-allnightmusic")
+html="<meta charset=\"utf-8\">\n<style>\n\ta{text-decoration:unset}\n</style>"
+import re
+import time
+for url in[a.get_attribute("name")for a in reversed(edge.find_elements_by_css_selector(".nooptg>a"))]:
+	time.sleep(.25)
+	edge.get(url)
+	for tracks in reversed(edge.find_elements_by_css_selector(".text")):
+		tracks=tracks.text
+		tracks=re.split("\n+",tracks)
+		tracks=[re.sub("\d+\.|M-\d+：|」$","",track)for track in tracks]
+		tracks=[track.replace("〜","~")for track in tracks]
+		tracks=[track.translate(str.maketrans({chr(n+65281):chr(n+33)for n in range(94)}))for track in tracks]
+		tracks=[[track.replace("+","%2B"),track]for track in tracks]
+		tracks=[[re.sub("[ 　]*[/／][ 　]*「?|[ 　]*「|[ 　]+♪","+",track[0]),re.sub("[ 　]*[/／][ 　]*「?|[ 　]*「|[ 　]+♪"," - ",track[1])]for track in tracks]
+		tracks=[[re.sub("[ 　]+","+",track[0]),re.sub("[ 　]+"," ",track[1])]for track in tracks]
+		tracks=["<a href=\"http:youtube.com/results?search_query="+track[0]+"\" target=\"_blank\">"+track[1]+"</a>"for track in tracks]
+		tracks="<br>\n\t".join(tracks)
+		html+="\n<p>\n\t"+tracks+"\n</p>"
+edge.quit()
+open("All Night Music.html","w",encoding="utf-8").write(html)
---
+++ Python/atGP_URLリスト.py
@@ -0,0 +1,23 @@
+type=input("1:事務等 2:IT 3:製造等 ")
+if type in["1","2","3"]:
+	from selenium import webdriver
+	edge=webdriver.Edge("msedgedriver.exe")
+	list=[]
+	i=1
+	while True:
+		edge.get(
+			"http:atgp.jp/search/top/search_result?"+(
+				"job_categories=b01001610000002000,b01001610000004000&job_types=b01001620000034000,b01001620000036000,b01001620000037000,b01001620000043000"
+					if type=="1"else
+				"job_categories=b01001610000005000&job_types=b01001620000035000"
+					if type=="2"else
+				"job_types=b01001620000027000,b01001620000029000,b01001620000031000,b01001620000033000,b01001620000041000"
+			)+"&recruit_type=0&page="+str(i)
+		)
+		for url in edge.find_elements_by_css_selector(".c-button--blue:not(.u-mr--10)"):
+			list.append(url.get_attribute("href").replace("https://www.","").lower())
+		if str(i)==edge.find_elements_by_css_selector(".c-pagenavi-c__number:last-of-type")[0].text:
+			break
+		i+=1
+	edge.quit()
+	open("atGP 事務等.txt"if type=="1"else"atGP IT.txt"if type=="2"else"atGP 製造等.txt","w").write("\n".join(sorted(list)))
